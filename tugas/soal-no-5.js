31323e929df8036f61087b4c98ade364305b9469
reynoalief <118473186+reynoalief@users.noreply.github.com>
2022-12-09 08:56:47 +0700

    Create soal-no-5.js

--- tugas/soal-no-5.js
+++ tugas/soal-no-5.js
@@ -2,14 +2,14 @@ function ubahKonsonan(hurufTarget, hurufPengganti, kalimatsebelum) {
   if (typeof kalimatsebelum !== typeof "string") {
     console.log("Tipe data harus String");
   } else {
-    let a = kalimatsebelum;
+    let a = kalimatsebelum.toLowerCase();
     console.log("Kalimat sebelumnya = " + a);
     console.log(
       "Kalimat sesudah = " + a.replaceAll(hurufTarget, hurufPengganti)
     );
   }
 }
-ubahKonsonan("a", "i", "saya cinta bu Mega");
+ubahKonsonan("a", "i", "RAynoalief Ridwan Ar Rhojabi");
 
 // let x = typeof "1223";
 // console.log(x);
