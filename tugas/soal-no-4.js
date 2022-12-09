ea599e900d46d251186ae774b81919c5637dc9fa
reynoalief <118473186+reynoalief@users.noreply.github.com>
2022-12-09 09:28:34 +0700

    Update soal-no-4.js

--- tugas/soal-no-4.js
+++ tugas/soal-no-4.js
@@ -3,18 +3,20 @@ function checkPalindrome(string) {
     //jika parameter kurang dari 3
     console.log("Teks atau angka harus lebih dari dua suku/angka");
   } else {
-    const len = string.length; //menentukan panjang huruf
+    const lowercase = string.toLowerCase().replace(/\s/g, "");
+
+    const len = lowercase.length; //menentukan panjang huruf
     for (let i = 0; i < len / 2 - 1; i++) {
       // lakukan jika 0 < panjang huruf
-      if (string[i] !== string[len - 1 - i]) {
+      if (lowercase[i] !== lowercase[len - 1 - i]) {
         // jika parameter ke indeks 0 tidak sama dengan parameter ke indeks 6
         //
-        console.log("It is not a palindrome"); // maka bukan termasuk palindrome
+        return console.log("It is not a palindrome"); // maka bukan termasuk palindrome
       }
     }
-    console.log("It is a palindrome"); // jika parameter ke indeks 0  sama dengan parameter ke indeks 6 dan
+    return console.log("It is a palindrome"); // jika parameter ke indeks 0  sama dengan parameter ke indeks 6 dan
     //parameter 1 sama dengan 5 dan seterusnya maka dinyatakan palindrome
   }
 }
 
-checkPalindrome("racecar");
+checkPalindrome("ka ta k");
