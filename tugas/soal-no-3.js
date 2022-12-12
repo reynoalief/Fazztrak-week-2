e0778d5dc844d89b7eb108e5108c531e1d73f507
reynoalief <118473186+reynoalief@users.noreply.github.com>
2022-12-12 17:37:56 +0700

    Update soal-no-3.js

--- tugas/soal-no-3.js
+++ tugas/soal-no-3.js
@@ -1,31 +1,31 @@
-const mtk = 70;
-const bi = 70;
-const bing = 70;
-const ipa = 70;
-
 function gradeUN(mtk, bi, bing, ipa) {
-  if (mtk && bi && bing && ipa == null) {
+  let result = "";
+
+  let matpel = (mtk, bi, bing, ipa);
+  let rataRata =
+    (parseInt(mtk) + parseInt(bi) + parseInt(bing) + parseInt(ipa)) / 4;
+  if (matpel == null || matpel == "") {
     //jika terdapat salah satu nilai kosong maka bernilai false
-    console.log("semua nilai harus di isi");
+    result = "semua nilai harus di isi";
+  } else if (isNaN(matpel)) {
+    result = "semua nilai harus number";
   } else {
-    // jika nilai tidak false maka lanjut ke perintah selanjutnya
-    let rataRata = (mtk + bi + bing + ipa) / 4;
-    console.log(rataRata);
     if (rataRata >= 90) {
       //jika rataRata lebih dari samadengan 90 hasil A - 100
-      console.log("Grade A");
+      result = "Grade A";
     } else if (rataRata >= 80) {
       //jika rataRata lebih dari samadengan 80 - 89
-      console.log("Grade B");
+      result = "Grade B";
     } else if (rataRata >= 70) {
       //jika rataRata lebih dari samadengan 70 - 79
-      console.log("Grade C");
+      result = "Grade C";
     } else if (rataRata >= 60) {
       //jika rataRata lebih dari samadengan 60 - 69
-      console.log("Grade D");
+      result = "Grade D";
     } else {
-      console.log("Grade E"); //jika tidak memenuhi kondisi
+      result = "Grade E"; //jika tidak memenuhi kondisi
     }
   }
+  return `nilai rata rata = ${rataRata}\nmendapatkan ${result}`;
 }
-gradeUN(95, 95, 95, 70); //memasukan 4 nilai mtk,bi,bing,ipa
+console.log(gradeUN(95, 95, 95, "88")); //memasukan 4 nilai mtk,bi,bing,ipa
